550a2b16a48f2d9f7729e9328bb8767389605fb3
jcpsimmons <jcpsimmons@gmail.com>
2020-02-11 11:59:31 -0800

    1 file modified

--- components/pages/About.js
+++ components/pages/About.js
@@ -3,8 +3,9 @@ import React from 'react'
 export default function About() {
   return(
     <div classname="mx-auto">
-    <h1>This is an about page</h1>
-    <p>it tells dad jokes and uses functional components with react hooks...and the react router</p>
+    <h1>About</h1>
+    <p>I made this app to experiment with functional components and react hooks (as opposed to building it with class based). The react router is used for the two pages. See more of my work at <a href="https://jcsdesign.me">jcsdesign.me</a>
+    </p>
     </div>
   )
 }
